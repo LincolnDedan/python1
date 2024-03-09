9b47438a62318ac7eb1976d9958b39b4715bcf6c
Northrift.git <peterbarasa600@gmail.com>
2024-03-09 13:35:55 +0300

    user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+location = input("Where do you live? ")
+salary=int(input("how much do you earn? "))
+department=input("which department do you work with? ")
+print(f"Hello {name}, you are {age} years, coming from {location},earning {salary}, from {department}.")
